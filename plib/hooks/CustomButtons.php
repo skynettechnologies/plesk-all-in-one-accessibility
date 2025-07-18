b7a22ba274d0a705e235c02dcb740bf0a7ff8be1
iOS Developer <developer3@skynettechnologies.com>
2025-07-18 19:17:51 +0530

    Custom Button hook is added for All in One Accessibility Menu.

---
+++ plib/hooks/CustomButtons.php
@@ -0,0 +1,38 @@
+<?php
+// Copyright 1999-2021. Plesk International GmbH.
+//PleskAllInOneAccessibility
+class Modules_PleskAllInOneAccessibility_CustomButtons extends pm_Hook_CustomButtons
+{
+
+    public function getButtons()
+    {
+        return [
+            [
+                'place' => self::PLACE_ADMIN_NAVIGATION,
+                'section' => self::SECTION_NAV_SERVER_MANAGEMENT,
+                'title' => 'All in One Accessibility',
+                'description' => 'Quick Web Accessibility Implementation with All In One Accessibility!',
+                'icon' => pm_Context::getBaseUrl() . 'icons/aioa-icon-type-1.svg',
+                'link' => pm_Context::getBaseUrl()
+            ],
+            [
+                'place' => self::PLACE_HOSTING_PANEL_NAVIGATION,
+                'title' => 'All in One Accessibility',
+                'description' => 'Quick Web Accessibility Implementation with All In One Accessibility!',
+                'icon' => pm_Context::getBaseUrl() . 'icons/aioa-icon-type-1.svg',
+                'link' => pm_Context::getBaseUrl()
+            ],
+            [
+                'place' => self::PLACE_DOMAIN,
+                'title' => 'All in One Accessibility',
+                'description' => 'Quick Web Accessibility Implementation with All In One Accessibility!',
+                'icon' => pm_Context::getBaseUrl() . 'icons/aioa-icon-type-1.svg',
+                'link' => pm_Context::getBaseUrl(),
+                'newWindow' => false,
+                'contextParams' => true,
+                'visibility' => [$this],
+            ],
+        ];
+    }
+
+}
